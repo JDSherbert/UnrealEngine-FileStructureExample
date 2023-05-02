f7430e9c0379d782ebda93130bb31f4c358d08b9
JDSherbert <jdsherbert@outlook.com>
2023-05-02 20:43:14 +0100

    Initial Commit.

---
+++ Source/FileStructureExample/FileStructureExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FileStructureExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FILESTRUCTUREEXAMPLE_API AFileStructureExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
